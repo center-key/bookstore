872e93c5301486277df872a7a11d434e7c9716da
Dem Pilafian <dem@centerkey.com>
2015-06-02 01:50:26 -0700

    Security interceptor to force authentication

---
+++ grails-app/controllers/org/centerkey/grails/security/SecurityInterceptor.groovy
@@ -0,0 +1,22 @@
+package org.centerkey.grails.security
+
+class SecurityInterceptor {
+
+   SecurityInterceptor() {
+      matchAll()
+         .excludes(uri: "/")
+         .excludes(uri: "/assets/**")
+         .excludes(controller: "security")
+      }
+
+   boolean before() {
+      session.securityRedirect = request.forwardURI
+      if (!session.securityUserId)
+         response.setHeader("Refresh", "0; url=/signin")  //TEMPORARY
+         //redirect(controller: "security", action: "signin")
+         //see: https://github.com/grails/grails-core/issues/600
+         //see: https://github.com/grails/grails-core/issues/625
+      return !!session.securityUserId
+      }
+
+}
