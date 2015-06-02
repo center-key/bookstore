4a2824e1befd27a198e0cab4c4bee911916ddf71
Dem Pilafian <dem@centerkey.com>
2015-06-02 01:41:32 -0700

    Security service methods register() and authenticate()

--- grails-app/services/org/centerkey/grails/security/SecurityService.groovy
+++ grails-app/services/org/centerkey/grails/security/SecurityService.groovy
@@ -25,9 +25,9 @@ class SecurityService {
          user = new User(email: email, hash: hash).save(flush: true)
          def empower = { new Capability(userId: user.id as String, roleId: it).save(flush: true) }
          Role.list().each { empower(it.id as String) }
-         println("\n*** Initial setup user created ***")
-         println("      User: ${email}")
-         println("   Pasword: ${password}")
+         log.println("\n*** Initial setup user created ***")
+         log.println("      User: ${email}")
+         log.println("   Pasword: ${password}")
          }
       def deleteUser = {
          Capability.findAllByUserId(user.id as String).each { it.delete(flush: true) }
@@ -40,11 +40,11 @@ class SecurityService {
       }
 
    def printSummary() {
-      println("\nSecurity Summary")
-      println("   Roles:")
-      Role.list().each { println("      ${it.id} - ${it.name}") }
-      println("   Counts:")
-      println("      roles (${Role.count()}), users (${User.count()}), capabilities (${Capability.count()})\n")
+      log.println("\nSecurity Summary")
+      log.println("   Roles:")
+      Role.list().each { log.println("      ${it.id} - ${it.name}") }
+      log.println("   Counts:")
+      log.println("      roles (${Role.count()}), users (${User.count()}), capabilities (${Capability.count()})\n")
       }
 
    def bootstrap() {
@@ -52,12 +52,34 @@ class SecurityService {
       setupInitialUser()
       printSummary()
       ////////////////////////////
-      println("---\nObjects:")
-      Role.list().each { println("   Role ${it.id} - ${it.name}") }
-      User.list().each { println("   User ${it.id} - ${it.email}") }
-      Capability.list().each { println("   ${User.get(it.userId).email} - ${Role.get(it.roleId).name}") }
-      println("---")
+      log.println("---\nObjects:")
+      Role.list().each { log.println("   Role ${it.id} - ${it.name}") }
+      User.list().each { log.println("   User ${it.id} - ${it.email}") }
+      Capability.list().each { log.println("   ${User.get(it.userId).email} - ${Role.get(it.roleId).name}") }
+      log.println("---")
       ////////////////////////////
+      }
+
+   def cleanupEmail(String email) {
+      return email.trim().toLowerCase()
+      }
+
+   def register(String email, String password) {
+      def user = new User(email: email, hash: encoder.encode(password))
+      user.save(flush: true)
+      def role = Role.findByCode(grailsApplication.config.security.newUserDefaultRole)
+      new Capability(userId: user.id as String, roleId: role.id as String).save(flush: true)
+      return user
+      }
+
+   def authenticate(String email, String password) {
+      def user = User.findByEmail(cleanupEmail(email))
+      return user && encoder.matches(password.trim(), user.hash) ? user : null
+      }
+
+   def setPassword(User user, String password) {
+      user.hash = encoder.encode(password.trim())
+      return user.save(flush: true)
       }
 
 }
